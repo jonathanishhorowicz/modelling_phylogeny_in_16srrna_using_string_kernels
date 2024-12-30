7ba3814c2e5b29659cd9cee741fbe05b36001b0d
Jonathan Ish-Horowicz <jonathan_ish@hotmail.co.uk>
2024-12-30 14:54:51 +0000

    add gp runner shell script

---
+++ run/run_gp_simulations.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+export PBS_JOBID_ROOT=local_test
+export N_REPLICATES=3
+export N_REP_BATCHES=1
+
+export TASK='regression'
+for PBS_ARRAY_INDEX in {0..11}; do
+	export PBS_JOBID="${PBS_JOBID_ROOT}[${PBS_ARRAY_INDEX}]"
+	export PBS_ARRAY_INDEX
+	echo $PBS_JOBID
+	python ../scripts/run_gp_simulation_replicates.py
+done
+
+export TASK='classification'
+for PBS_ARRAY_INDEX in {0..5}; do
+	export PBS_JOBID="${PBS_JOBID_ROOT}[${PBS_ARRAY_INDEX}]"
+	export PBS_ARRAY_INDEX
+	echo $PBS_JOBID
+	python ../scripts/run_gp_simulation_replicates.py
+done
+
